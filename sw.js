13c67c36c2c786183d941d337ff08a46b9e5ff21
Smasharn-UMA <the-stretch-runner_b-a.k-6_o-m@i.softbank.jp>
2026-07-24 19:51:12 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,13 @@
-const CACHE_NAME="horse-evaluator-v201";
-const ASSETS=["./","./index.html?v=201","./style.css?v=201","./app.js?v=201","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll({type:"window"}))
+      .then(clients => Promise.all(clients.map(client => client.navigate(client.url))))
+  );
+});
+self.addEventListener("fetch", () => {});
